9adb2bab905b82358d217594a1abf55240f3e46d
MariahRojas <51241518+MariahRojas@users.noreply.github.com>
2019-08-23 15:06:38 -0400

    made app.js updates. to be continued

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const epxpress = require('express')
-const app = epxpress()
+const express = require('express')
+const app = express()
 
+/* data.json file */
+const { books } = require('./data.json');
 
+/* middleware */
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
 
+/* routes */
+app.get('/', function (req, res) {
 
+})
 
 
 
