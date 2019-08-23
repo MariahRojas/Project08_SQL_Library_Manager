ce6d9187efb79134723a95070e18f8ff534a3bbb
MariahRojas <51241518+MariahRojas@users.noreply.github.com>
2019-08-23 13:36:26 -0400

    updates
    Installed
    - express
    - pug
    - sequelize
    - sqlite3
    - express-validator
    - sequelize-cli
    
    update config file
    created views and puf files

---
+++ app.js
@@ -0,0 +1,22 @@
+const epxpress = require('express')
+const app = epxpress()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const portNumber = 3000
+
+app.listen(portNumber)
+console.log('App started on a localhost at port ' + portNumber);
