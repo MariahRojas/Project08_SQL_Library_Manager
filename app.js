458c1fcdf0c94381815c89814abc57d40616fa58
MariahRojas <51241518+MariahRojas@users.noreply.github.com>
2019-08-26 13:09:07 -0400

    Updates
    >New book route is rendered.
    >Create new book console logs submit
    >Cancel button directs to home route

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const path = require("path");
+const bodyParser = require('body-parser');
 
 //Initialize App
 const app = express();
 
+//Bring in Models
+let Books = require('./models/books')
+
+/* Middleware */
 //Load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use('/static', express.static('public'))
+
+//Body Parser Middleware
+//parse application/x-wwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+//parse application/json
+app.use(bodyParser.json())
 
 //Home Route
 app.get('/', function(req, res){
     res.render('index')
 })
+
+
+//Add Route
+app.get('/books/new', function(req, res){
+    res.render('new-book')
+})
+
+//Add Submit POST Route
+app.post('/books/new', function(req, res){
+    console.log('Submitted'); //Submiting to the server
+    return;
+})
+
+
 
 
 
